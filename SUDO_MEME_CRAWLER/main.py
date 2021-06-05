348728ab09ef4195dc02956af4c5e13ea3ef96c8
vkSupervillain <68009254+vkSupervillain@users.noreply.github.com>
2021-06-05 12:29:54 +0200

    Popravil upload crawlerja
    Pri prejšnem commitu je nekaj šlo narobe!!

--- SUDO_MEME_CRAWLER/main.py
+++ SUDO_MEME_CRAWLER/main.py
@@ -9,16 +9,16 @@ import shutil
 import os
 
 def download_web_image(url, directory):
-    r = requests.get(url, stream=True)
+    r = requests.get(url, stream=True) # request za sliko
     if r.status_code == 200:
-        name = directory + '/' +  url.replace("/","")
-        with open(name, 'wb') as f:
+        name = directory + '/' +  url.replace("/","") # Sliko dam v direktory, ker pa ne sme imeti datoteka v imenu / samo replaceam z praznim stringom
+        with open(name, 'wb') as f: # shranjevanje
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
 
 
 
-def is_number(s):
+def is_number(s): # simpl funkcija za preverjanje če je številka
     try:
         float(s)
         return True
@@ -29,19 +29,21 @@ def spider(plants):
     http = httplib2.Http()
     for j in range (0,len(plants)):
         try:
-            os.mkdir(plantsNames[j])
+            os.mkdir(plantsNames[j]) # Ustvarim direktorij z imenom
         except:
             print("Couldnt create directory")
         status, response = http.request('https://garden.org/plants/view/' + plants[j])
-        for link in BeautifulSoup(response, 'html.parser', parse_only=SoupStrainer('img')):
+        for link in BeautifulSoup(response, 'html.parser', parse_only=SoupStrainer('img')): # najdem vse slike
             if link.has_attr('class'):
-                if(link['class'] == ['plant_thumbimage'] and link['height']=="175"):
+                if(link['class'] == ['plant_thumbimage'] and link['height']=="175"): # Vse slike v databaseu ki me zanimajo so classa plant_thumbimage in heighta 175
                     print(link)
-                    download_web_image("https://garden.org" + link['src'].replace("-175",""), plantsNames[j])
+                    download_web_image("https://garden.org" + link['src'].replace("-175",""), plantsNames[j]) # Full sized sliko dobim da zamenjam -175 z praznim stringom.
 
 
 if __name__ == "__main__":
-    plants = ["181473", "181474"]
-    plantsNames = ["Daylilies", "Iris"]
+    # med plants zapišeš številko database entry-ja npr https://garden.org/plants/view/228296
+    plants = ["181506", "76165", "186377", "228296"]
+    # med plantsNames pa na n-to mesto zapišeš ime kako naj bo tvoj entry poimenovan
+    plantsNames = ["Roses", "Dalhias", "Sempervivum", "Salvias"]
     x = spider(plants)
 
