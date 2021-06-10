120bbf9d7a5755e7cb49e0af1d6dfef54891c9fb
vkSupervillain <68009254+vkSupervillain@users.noreply.github.com>
2021-06-10 23:19:03 +0200

    Posodobil trainer-ja

--- SUDO_MEME_TRAINER/trainer.py
+++ SUDO_MEME_TRAINER/trainer.py
@@ -30,7 +30,7 @@ def napolniTrainingInTestingImages(potDoDatabasea, imgWidth, imgHeight):
 if __name__ == "__main__":
     imgWidth = 400 # width slike pri učenju modela
     imgHeight = 400 # height slike pri učenju modela
-    potDoDatabasea="/python/TRAINER/DATASET" # pot do dataseta ki ga želimo uporabit.
+    potDoDatabasea="/home/besko/Desktop/FOTOsinteza-main/SUDO_MEME_TRAINER/dataset" # pot do dataseta ki ga želimo uporabit.
     # Dataset ima strukturo -DATASET
     #                           -CLASS1
     #                               -image in class1
@@ -44,17 +44,21 @@ if __name__ == "__main__":
     # Narejeno s pomočjo https://www.tensorflow.org/tutorials/load_data/images ter https://www.youtube.com/watch?v=t0EzVCvQjGE
     model = models.Sequential()
     model.add(layers.experimental.preprocessing.Rescaling(1./255)) # rescaleam slik
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(imgHeight, imgWidth, 3))) # Input layer, input shape je slika heightxwidth ter 3 channels(barve)
-    model.add(layers.MaxPooling2D((2, 2))) # Maxpooling layer ki poenostavi rezultate activation layerja
-    model.add(layers.Conv2D(64, (3, 3), activation='relu')) # Activation relu layer
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(16, 3, activation='relu', input_shape=(imgHeight, imgWidth, 3))) # Input layer, input shape je slika heightxwidth ter 3 channels(barve)
+    model.add(layers.MaxPooling2D()) # Maxpooling layer ki poenostavi rezultate activation layerja
+    model.add(layers.Conv2D(32, 3, activation='relu')) # Activation relu layer
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Conv2D(64, 3, activation='relu'))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Conv2D(128, 3, activation='relu'))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Conv2D(256, 3, activation='relu'))
     model.add(layers.Flatten()) # Iz matrike naredimo enodimenzionalen array
-    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(512, activation='relu'))
     model.add(layers.Dense(num_classes,activation='softmax')) # Output layer, softmax normalizira rezultate med 0-1
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']) # Compilamo model, gledamo natančnost
-    model.fit(training_images, epochs=10, validation_data=validation_images) # Poženemo model, epochs je št. ponovitev
+    model.fit(training_images, epochs=4, validation_data=validation_images) # Poženemo model, epochs je št. ponovitev
 
     loss, accuracy = model.evaluate(validation_images) # ocenim loss ter accuracy modela.
     print("Loss: ")
